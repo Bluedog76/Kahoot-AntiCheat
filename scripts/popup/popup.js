844b9b106c296c647564e48da549e7f003da81f8
Nathan <bluedoghusky27@gmail.com>
2023-05-03 20:41:30 -0500

    v0.01

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -1,8 +1,10 @@
 function save() {
-    var newItem = document.getElementById("box").ariaValueMax;
+    var newItem = document.getElementById("box").value;
     localStorage.box = newItem;
 }
 function load() {
     var savedDiv = document.getElementById("savedList")
     savedDiv.innerHTML = localStorage.box;
 }
+document.getElementById("save").addEventListener("click", save);
+document.getElementById("load").addEventListener("click", load);
