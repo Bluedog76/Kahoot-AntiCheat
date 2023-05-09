73496323b7a600ef7f4b97fd39fc84692c2129b4
Bluedog76 <52477795+Bluedog76@users.noreply.github.com>
2023-05-09 13:16:07 +0000

    v0.01

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 //To find an element using the css name instade of using an ID.
 
 while (true) {
-    var Name = document.getElementsByClassName('controller-entry__ControllerName-sc-sqj4to-1 onogI')[0];
-    console.log(Name)
+    let elements = document.querySelectorAll(':not([id])');
+    console.log(elements)
 }
