9394611db210e222a6d266aafc222b63e14d363d
Bluedog76 <52477795+Bluedog76@users.noreply.github.com>
2023-05-12 13:44:40 +0000

    v0.01

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,13 @@
 //This is a content script running indavidualy on play.kahot.it
 
-
-let itemId = document.querySelectorAll(':not([id])');
-var port = chrome.runtime.connect({name: "idSend"});
-port.postMessage({item: itemId});
-//port.onMessage.addListener(function(msg) {
-  //if (msg.question === "Who's there?")
-    //port.postMessage({answer: "Madame"});
-  //else if (msg.question === "Madame who?")
-    //port.postMessage({answer: "Madame... Bovary"});
-//};
-
+for (var i = 0; i < Infinity; i++) {
+  let itemId = document.querySelectorAll(':not([id])');
+  var port = chrome.runtime.connect({name: "idSend"});
+  port.postMessage({item: itemId});
+  //port.onMessage.addListener(function(msg) {
+    //if (msg.question === "Who's there?")
+      //port.postMessage({answer: "Madame"});
+    //else if (msg.question === "Madame who?")
+      //port.postMessage({answer: "Madame... Bovary"});
+  //};
+}
--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -13,11 +13,12 @@ document.getElementById("load").addEventListener("click", load);
 
 
 //Send and recive to the content script (Host)
-
-chrome.runtime.onConnect.addListener(function(port) {
-    console.assert(port.name === "idSend");
-    port.onMessage.addListener(function(msg) {
-        var savedDiv2 = document.getElementById("savedList2")
-        savedDiv2.innerHTML = msg.item;
+for (var i = 0; i < Infinity; i++) {
+    chrome.runtime.onConnect.addListener(function(port) {
+        console.assert(port.name === "idSend");
+        port.onMessage.addListener(function(msg) {
+            var savedDiv2 = document.getElementById("savedList2")
+            savedDiv2.innerHTML = msg.item;
+            });
     });
-});
+}
