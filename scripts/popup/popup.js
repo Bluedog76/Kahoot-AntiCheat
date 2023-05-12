cc4a9019e6ae0cbb8ad56b6b95d66b6b363277ee
Bluedog76 <52477795+Bluedog76@users.noreply.github.com>
2023-05-12 15:24:00 +0000

    v0.01

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -16,7 +16,7 @@ document.getElementById("load").addEventListener("click", load);
 
 
 //Send and recive to the content script (Host)
-    sleep(5000)
+
     chrome.runtime.onConnect.addListener(function(port) {
         console.assert(port.name === "idSend");
         port.onMessage.addListener(function(msg) {
