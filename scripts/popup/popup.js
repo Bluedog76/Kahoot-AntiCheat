1b6a83dda36c4da8b7108e0ccb9d88d7057e6882
Bluedog76 <52477795+Bluedog76@users.noreply.github.com>
2023-05-12 15:29:39 +0000

    v0.01

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -16,11 +16,10 @@ document.getElementById("load").addEventListener("click", load);
 
 
 //Send and recive to the content script (Host)
-
-    chrome.runtime.onConnect.addListener(function(port) {
-        console.assert(port.name === "idSend");
-        port.onMessage.addListener(function(msg) {
-            var savedDiv2 = document.getElementById("savedList2")
-            savedDiv2.innerHTML = msg.item;
-            });
-    });
+chrome.runtime.onConnect.addListener(function(port) {
+    console.assert(port.name === "idSend");
+    port.onMessage.addListener(function(msg) {
+        var savedDiv2 = document.getElementById("savedList2")
+        savedDiv2.innerHTML = msg.item;
+        });
+});
